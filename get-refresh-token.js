b2d955ee988f6c0e493cbe8b6e54b4161cf04f4b
gringoglobalchanel-debug <gringoglobalchanel@gmail.com>
2025-10-29 17:38:11 -0400

    Update get-refresh-token.js

--- get-refresh-token.js
+++ get-refresh-token.js
@@ -1,8 +1,8 @@
 const { google } = require('googleapis');
 
 // 🔑 REEMPLAZA CON TUS CREDENCIALES REALES
-const CLIENT_ID = 'tu-client-id-real.apps.googleusercontent.com';
-const CLIENT_SECRET = 'GOCSPX-tu-client-secret-real';
+const CLIENT_ID = "667720262345-0glb797o2bk786k8v88un6hleda7k3st.apps.googleusercontent.com","project_id":"bringoedu","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+const CLIENT_SECRET = "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","redirect_uris":["http://localhost:3000","https://bringo-edu-backend-2.onrender.com"
 const REDIRECT_URI = 'https://bringo-edu-backend-2.onrender.com';
 
 const oauth2Client = new google.auth.OAuth2(
